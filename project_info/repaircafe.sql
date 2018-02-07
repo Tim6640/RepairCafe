71da8a62ca16003be16d4d58c62f7366473a0d71
tvb <tim.van.beek@ivention.nl>
2018-02-07 09:11:43 +0100

    .sql file updated

--- project_info/repaircafe.sql
+++ project_info/repaircafe.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 01, 2018 at 10:57 AM
+-- Generation Time: Feb 07, 2018 at 09:09 AM
 -- Server version: 10.1.30-MariaDB
 -- PHP Version: 7.2.1
 
@@ -59,6 +59,13 @@ CREATE TABLE `employee` (
   `power` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `employee`
+--
+
+INSERT INTO `employee` (`employee_id`, `employee_name`, `password`, `e-mail`, `power`) VALUES
+(1, 'Tim', 'afw;f/vd', 'test.test@testmail.test', 1);
+
 -- --------------------------------------------------------
 
 --
@@ -77,6 +84,21 @@ CREATE TABLE `invoice` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `material`
+--
+
+CREATE TABLE `material` (
+  `material_id` int(11) NOT NULL,
+  `order_id` int(11) NOT NULL,
+  `material_name` text NOT NULL,
+  `supplier` text NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `amount` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `product`
 --
 
@@ -85,15 +107,16 @@ CREATE TABLE `product` (
   `product_name` text NOT NULL,
   `product_state` tinyint(1) NOT NULL,
   `serie_number` int(11) NOT NULL,
-  `issue` text NOT NULL
+  `info_tried` text NOT NULL,
+  `info_problem` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `product`
 --
 
-INSERT INTO `product` (`product_id`, `product_name`, `product_state`, `serie_number`, `issue`) VALUES
-(1, 'Test', 1, 1231, 'Test');
+INSERT INTO `product` (`product_id`, `product_name`, `product_state`, `serie_number`, `info_tried`, `info_problem`) VALUES
+(1, 'Test', 1, 1231, 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui.\r\n\r\nEtiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc, quis gravida magna mi a libero. Fusce vulputate eleifend sapien. Vestibulum purus quam, scelerisque ut, mollis sed, nonummy id, metus. Nullam accumsan lorem in dui. Cras ultricies mi eu turpis hendrerit fringilla. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; In ac dui quis mi consectetuer lacinia.\r\n\r\nNam pretium turpis et arcu. Duis arcu tortor, suscipit eget, imperdiet nec, imperdiet iaculis, ipsum. Sed aliquam ultrices mauris. Integer ante arcu, accumsan a, consectetuer eget, posuere ut, mauris. Praesent adipiscing. Phasellus ullamcorper ipsum rutrum nunc. Nunc nonummy metus. Vestibulum volutpat pretium libero. Cras id dui. Aenean ut eros et nisl sagittis vestibulum. Nullam nulla eros, ultricies sit amet, nonummy id, imperdiet feugiat, pede. Sed lectus. Donec mollis hendrerit risus. Phasellus nec sem in justo pellentesque facilisis. Etiam imperdiet imperdiet orci. Nunc nec neque. Phasellus leo dolor, tempus non, auctor et, hendrerit quis, nisi.', 'arcu, accumsan a, consectetuer eget, posuere ut, mauris. Praesent adipiscing. Phasellus ullamcorper ipsum rutrum nunc. Nunc nonummy metus. Vestibulum volutpat pretium libero. Cras id dui. Aenean ut eros et nisl sagittis vestibulum. Nullam nulla eros, ultricies sit amet, nonummy id, imperdiet feugiat, pede. Sed lectus. Donec mollis hendrerit risus. Phasellus nec sem in justo pellentesque facilisis. Etiam imperdiet imperdiet orci. Nunc nec neque. Phasellus leo dolor, tempus non, auctor et, hendrerit quis, nisi.');
 
 -- --------------------------------------------------------
 
@@ -106,11 +129,18 @@ CREATE TABLE `repair_order` (
   `customer_id` int(11) NOT NULL,
   `product_id` int(11) NOT NULL,
   `status` varchar(64) NOT NULL,
-  `costs` decimal(11,0) NOT NULL,
+  `costs` decimal(10,0) NOT NULL,
   `time_stamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `category` text NOT NULL,
   `employee_name` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `repair_order`
+--
+
+INSERT INTO `repair_order` (`order_id`, `customer_id`, `product_id`, `status`, `costs`, `time_stamp`, `category`, `employee_name`) VALUES
+(0, 1, 1, 'InProgress', '47', '2018-02-05 07:38:41', 'Software', 'Tim');
 
 --
 -- Indexes for dumped tables
@@ -135,6 +165,13 @@ ALTER TABLE `invoice`
   ADD PRIMARY KEY (`invoice_id`);
 
 --
+-- Indexes for table `material`
+--
+ALTER TABLE `material`
+  ADD PRIMARY KEY (`material_id`),
+  ADD KEY `FK_order_id` (`order_id`);
+
+--
 -- Indexes for table `product`
 --
 ALTER TABLE `product`
@@ -145,8 +182,8 @@ ALTER TABLE `product`
 --
 ALTER TABLE `repair_order`
   ADD PRIMARY KEY (`order_id`),
-  ADD KEY `customer_id` (`customer_id`),
-  ADD KEY `product_id` (`product_id`);
+  ADD KEY `FK_customer_id` (`customer_id`),
+  ADD KEY `FK_product_id` (`product_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -162,7 +199,7 @@ ALTER TABLE `customer`
 -- AUTO_INCREMENT for table `employee`
 --
 ALTER TABLE `employee`
-  MODIFY `employee_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `employee_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `invoice`
@@ -171,16 +208,27 @@ ALTER TABLE `invoice`
   MODIFY `invoice_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `material`
+--
+ALTER TABLE `material`
+  MODIFY `material_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
 -- AUTO_INCREMENT for table `product`
 --
 ALTER TABLE `product`
   MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT for table `repair_order`
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `repair_order`
 --
 ALTER TABLE `repair_order`
-  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  ADD CONSTRAINT `FK_customer_id` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`customer_id`),
+  ADD CONSTRAINT `FK_product_id` FOREIGN KEY (`product_id`) REFERENCES `product` (`product_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
